a9e1a0c1a5957f80ed5a1f5817049a146ecaf53a
miseon <bunny62420@gmail.com>
2023-09-20 10:22:15 +0900

    slide

--- script/script.js
+++ script/script.js
@@ -39,3 +39,24 @@ $('.c12 .title a:nth-child(2)').on('click',function(){
     $('.c2').show();
     $('.c1').hide();
 })
+
+// 슬라이드
+/* 이미지만 바뀌면 안 되고, 
+이미지가 위에서 아래 또는 아래에서 위로 이동하면서 전환되어야 한다.
+슬라이드는 매 3초 이내로 하나의 이미지에서 다른 이미지로 전환되어야 한다.
+웹사이트를 열었을 때 자동으로 시작되어 반복적으로
+(마지막 이미지가 슬라이드되면 다시 첫 번째 이미지가 슬라이드 되는 방식) 슬라이드 되어야한다. */
+let count = 0
+// 0, 300, 600px 로 높이값 반복
+setInterval(function(){
+    // console.log(count++)
+    count++ /* 0,1,2,3으로 증가 */
+    if(count>2){count=0}
+    let total = count * 300 /* (1*300, 2*300, 3*300 ..) */
+    console.log(total)
+    // slide이미지가 아닌 slide_contents 자체를 통째로 이동시키는 것
+    $('.slide_contents').css('transform',`translateY(-${total}px)`)
+    /* 고정이 아닌 변경이 있기 때문에 ``기호로 사용 */
+},3000) /* 3000=3s */
+/* 수평으로 이동은 css에서 이미지 수평으로 만들고
+count * 1200 , translateX 사용 */
